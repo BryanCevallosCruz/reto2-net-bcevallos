b8048780cd06b4c612c8a1d671f005075e593f18
BryanCevallosCruz <bryancevac@outlook.com>
2022-12-12 18:00:12 -0500

    logg

--- Services/CedulaNombreAppServices.cs
+++ Services/CedulaNombreAppServices.cs
@@ -1,6 +1,4 @@
 
-using Microsoft.Extensions.Logging;
-
 namespace reto2.net.bcevallos;
 
 public class CedulaNombreAppServices : ICedulaNombreAppServices
@@ -15,26 +13,35 @@ public class CedulaNombreAppServices : ICedulaNombreAppServices
     {
         var cedulaNombre = new Dictionary<string, string>()
         {
-            {"0504045678", "Raul Velasco"}, 
+            {"0504045678", "Raul Velasco"},
             {"1234567890", "Joaquin Romero"}
-        } ;
+        };
 
         logger.LogInformation("Obtener por cedula");
 
         var valor = cedulaNombre[cedula];
+        logger.LogInformation(valor);
         // 
-        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(valor);
-        
-        logger.LogInformation(System.Convert.ToBase64String(plainTextBytes));
+        if (valor == null)
+        {
+           var msg = $"No existe la cedula {cedula}";
+            logger.LogError(msg);
 
-        var resultado = new CedulaNombre();
-        resultado.Cedula=cedula;
-        resultado.Nombre=System.Convert.ToBase64String(plainTextBytes);
+            throw new ArgumentException(msg);
+        }
+        else
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(valor);
+
+            var resultado = new CedulaNombre();
+            resultado.Cedula = cedula;
+            resultado.Nombre = System.Convert.ToBase64String(plainTextBytes);
 
 
-        logger.LogInformation($"||METODO GET||Cedula:{System.Convert.ToBase64String(plainTextBytes)}||CODIGO 200");
-        logger.LogTrace($"||METODO GET||Cedula:{System.Convert.ToBase64String(plainTextBytes)}||CODIGO 200");
+            logger.LogInformation($"||METODO GET||Cedula:{System.Convert.ToBase64String(plainTextBytes)}||CODIGO 200");
+            logger.LogTrace($"||METODO GET||Cedula:{System.Convert.ToBase64String(plainTextBytes)}||CODIGO 200");
 
-        return resultado; 
+            return resultado;
+        }
     }
 }
